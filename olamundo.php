05ded32abaab1770133eefcb481730746b4fcdbc
siqueirabianca1 <169085660+siqueirabianca1@users.noreply.github.com>
2024-07-22 18:22:53 -0300

    Add files via upload

---
+++ olamundo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<h1> Minha primeira página em HTML</h1>
+<?php
+     echo "Olá mundo!";
+
+?>
+
+</body>
+</html>
